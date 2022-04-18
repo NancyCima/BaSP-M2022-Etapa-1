b8938b093a811a6e257b8051680ff086f831c25c
Nancy Cima <nancycimabertoni@gmail.com>
2022-04-18 20:44:05 -0300

    BaSP-2022: ex05-for

---
+++ Semana-05/exercises/05_for.js
@@ -0,0 +1,44 @@
+console.log('Exercise 5: For.');
+
+/* a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript
+para mostrar una alerta utilizando cada una de las palabras. */
+
+console.log('Excercise 5.a:');
+
+var dessertArray = ['lemon pie', 'cheesecake', 'chocotorta', 'ice cream', 'puddin'];
+for (var i = 0; i < dessertArray.length; i++) {
+    alert(dessertArray[i]);
+}
+
+/* b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una
+alerta por cada palabra modificada. */
+
+console.log('Excercise 5.b:');
+
+for (var i = 0; i < dessertArray.length; i++) {
+    alert(dessertArray[i].substring(0,1).toUpperCase() + dessertArray[i].substring(1));
+}
+
+/* c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo
+con un bucle for para ir guardando cada palabra dentro de la variable sentence.
+Al final mostrar una única alerta con la cadena completa. */
+
+console.log('Excercise 5.c:');
+
+var sentence = '';
+for (var i = 0; i < dessertArray.length; i++) {
+    sentence = sentence + '  ' + dessertArray[i];
+}
+alert(sentence);
+
+/* d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición,
+es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0
+hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log). */
+
+console.log('Excercise 5.d:');
+
+var arrayVacio = [];
+for (var i = 0; i < 10; i++) {
+    arrayVacio[i] = i;
+}
+console.log(arrayVacio);
